b21836d80d4255dade5c8faa65f1fda1e6196415
zinderud <mokosam@gmail.com>
2023-04-29 01:57:27 +0300

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LibService } from './lib/lib.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,27 @@ import { LibService } from './lib/lib.service';
 })
 export class AppComponent {
   title = 'celestiapfb';
-  constructor(private libService: LibService) {
+ 
+  constructor(private libService: LibService, private formGroup: FormGroup) {}
+  form =  new FormGroup({
+  namespace_id: new FormControl(''),
+  data: new FormControl('')
+});
 
+  ngOnInit() {
+   
   }
+    getpayload(namespace_id: any,data:any) {
+    return  this.libService.getdata(namespace_id, data).subscribe((data: any) => {
+        console.log(data);
+      });
+    }
+    onSubmit() {
+      console.log(this.form.value);
+      this.getpayload(this.form.value.namespace_id,this.form.value.data);
+      
+    }
+
+ 
 
 }
